a0426ba1eca508154f5acfc4f3f3afc5ea744fa7
bat2492 <bat2492@tamu.edu>
2025-02-24 09:58:16 -0600

    updated tailwind

---
+++ client/src/analytics/analytics.jsx
@@ -0,0 +1,11 @@
+import React, {useEffect, useState} from 'react'
+
+function Analytics() {
+    return(
+        <div>
+            <p>Analytics</p>
+        </div>
+    )
+}
+
+export default Analytics
