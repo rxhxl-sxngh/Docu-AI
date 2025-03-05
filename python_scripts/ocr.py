b764e4ea03a21775d1468a4da72cbd5a01c8f48b
bat2492 <bat2492@tamu.edu>
2025-03-04 18:13:45 -0600

    added scripts folder with ocr script

---
+++ python_scripts/ocr.py
@@ -0,0 +1,13 @@
+from paddleocr import PaddleOCR
+from matplotlib import pyplot as plt
+import sys
+
+def Process_File(file_path):
+    ocr = PaddleOCR(use_angle_cls=True, lang='en')
+    result = ocr.ocr(file_path, cls=True)
+    print(result)
+
+
+if __name__ == '__main__':
+    fname = sys.argv[1]
+    Process_File(fname)
