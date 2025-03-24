ed2f5c85bcdf2a644b951a519e17e5d5288d764e
bat2492 <bat2492@tamu.edu>
2025-03-24 10:11:25 -0500

    ocr model

--- python_scripts/ocr.py
+++ python_scripts/ocr.py
@@ -1,9 +1,10 @@
+import fitz
 from paddleocr import PaddleOCR
 import sys
 
 def Process_File(file_path):
     ocr = PaddleOCR(use_angle_cls=True, lang='en')
-    result = ocr.ocr(file_path, cls=True)
+    result = ocr.ocr(file_path, det=False)
     print(result)
 
 
