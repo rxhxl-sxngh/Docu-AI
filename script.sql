90d479e970a9834f9b288f1f02e34c0228f913ce
samad649 <122996349+samad649@users.noreply.github.com>
2025-03-24 10:18:57 -0500

    schema for Docu AI database

---
+++ script.sql
@@ -0,0 +1,59 @@
+﻿CREATE TABLE Invoices (
+    id SERIAL PRIMARY KEY,
+    created_by INT,
+    modified_by INT,
+    created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    modified_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE Users (
+    id SERIAL PRIMARY KEY,
+    created_by INT,
+    created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    user_id INT UNIQUE,
+    first_name VARCHAR(255)
+);
+
+
+CREATE TABLE Documents (
+    document_id SERIAL PRIMARY KEY,
+    uploaded_by INT,
+    modified_by INT,
+    uploaded_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    modified_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE System_Settings (
+    system_settings_id SERIAL PRIMARY KEY,
+    created_by INT,
+    modified_by INT,
+    created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    modified_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE Processing_Queue (
+    id SERIAL PRIMARY KEY,
+    created_by INT,
+    modified_by INT,
+    created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    modified_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE Processing_Results (
+    id SERIAL PRIMARY KEY,
+    created_by INT,
+    modified_by INT,
+    created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    modified_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE System_Logs (
+    id SERIAL PRIMARY KEY,
+    created_by INT,
+    modified_by INT,
+    created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    modified_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
