55c6d16931dd88abae461930c9852e1b5ac0f07d
rxhxl-sxngh <rsingh032503@gmail.com>
2025-04-05 18:01:49 -0500

    crud operations for users

--- server/app/crud/user.py
+++ server/app/crud/user.py
@@ -16,8 +16,7 @@ class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
         db_obj = User(
             email=obj_in.email,
             hashed_password=get_password_hash(obj_in.password),
-            first_name=obj_in.first_name,
-            last_name=obj_in.last_name,
+            full_name=obj_in.full_name,
             is_superuser=obj_in.is_superuser,
         )
         db.add(db_obj)
