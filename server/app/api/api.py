366cd487d97a78b84b37783ddd4b272d05a39409
rxhxl-sxngh <rsingh032503@gmail.com>
2025-04-06 23:29:38 -0500

    updated API router to include new status endpoint

--- server/app/api/api.py
+++ server/app/api/api.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.endpoints import documents, queue, results, users, login
+from app.api.endpoints import documents, queue, results, users, login, status
 
 api_router = APIRouter()
 api_router.include_router(login.router, prefix="/login", tags=["login"])
@@ -8,3 +8,4 @@ api_router.include_router(documents.router, prefix="/documents", tags=["document
 api_router.include_router(queue.router, prefix="/queue", tags=["queue"])
 api_router.include_router(results.router, prefix="/results", tags=["results"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
+api_router.include_router(status.router, prefix="/status", tags=["status"])
