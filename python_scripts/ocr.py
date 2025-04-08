1a06194b03c4edb1e19a9fe0664d982f7898135d
bat2492 <bat2492@tamu.edu>
2025-04-08 11:12:08 -0500

    Brandon python script update

--- python_scripts/ocr.py
+++ python_scripts/ocr.py
@@ -1,11 +1,17 @@
 import fitz
 from paddleocr import PaddleOCR
 import sys
+import json
 
 def Process_File(file_path):
     ocr = PaddleOCR(use_angle_cls=True, lang='en')
-    result = ocr.ocr(file_path, det=False)
-    print(result)
+    result = ocr.ocr(file_path, cls=True)[0]
+    #print(result)
+    obj = [{f"{x[1][0]}":x[1][1]} for x in result]
+    #print(obj[0])
+    #print(obj)
+    print(json.dumps(obj,separators=(',', ':')))
+
 
 
 if __name__ == '__main__':
